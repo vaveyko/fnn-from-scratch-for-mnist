6f85c4c5429759c023fa2155565e239c8fd198e8
vaveyko <zakhvey.ivan@gmail.com>
2025-11-25 12:23:09 +0300

    fix(fnn): change shape of weights matrix

--- FeedforwardNN.py
+++ FeedforwardNN.py
@@ -17,8 +17,8 @@ class FeedforwardNN:
 
     def _init_weights(self, layers_size: List[int,], is_norm_dist: bool = True) -> None:
         for i in range(1, len(layers_size)):
-            self.weights.append(np.ndarray(shape=(layers_size[i], layers_size[i-1])))
-            size = (layers_size[i], layers_size[i-1])
+            size = (layers_size[i-1], layers_size[i])
+            np.random.seed(42)
             matrix = np.random.normal(0, 1, size) if is_norm_dist else np.random.uniform(-1, 1, size)
             self.weights.append(matrix)
 

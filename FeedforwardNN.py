c4051cdd9cea8358f3fb813e93e44b4eef3d4239
vaveyko <zakhvey.ivan@gmail.com>
2025-11-25 11:13:55 +0300

    feat(fnn): add option to change distribution for weights initialization

--- FeedforwardNN.py
+++ FeedforwardNN.py
@@ -15,6 +15,10 @@ class FeedforwardNN:
     def predict(self):
         pass
 
-    def _init_weights(self, layers_size: List[int,]) -> None:
+    def _init_weights(self, layers_size: List[int,], is_norm_dist: bool = True) -> None:
         for i in range(1, len(layers_size)):
             self.weights.append(np.ndarray(shape=(layers_size[i], layers_size[i-1])))
+            size = (layers_size[i], layers_size[i-1])
+            matrix = np.random.normal(0, 1, size) if is_norm_dist else np.random.uniform(-1, 1, size)
+            self.weights.append(matrix)
+
